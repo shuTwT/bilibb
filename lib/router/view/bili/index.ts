c693cde54e02ca4a5e60a2009a06917be5505bcd
shuTwT <1324073644@qq.com>
2024-02-23 15:43:29 +0800

    chore: update

--- lib/router/view/bili/index.ts
+++ lib/router/view/bili/index.ts
@@ -2,8 +2,6 @@ import path from "node:path"
 import Router from "koa-router";
 import ejs from "ejs";
 import { str2num, parseQuery, getTemplate } from "../../api/utils.js";
-import {createSSRApp,defineAsyncComponent} from "vue"
-import {renderToString} from "vue/server-renderer"
 import type { Context, DefaultState } from "koa";
 
 const biliRouter =new Router<DefaultState, Context>({prefix:"/bili"})
