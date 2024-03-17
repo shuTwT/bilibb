bd6b64d681233b5841d54485fd24d0b181f9bd63
shu <1324073644@qq.com>
2024-03-17 21:16:40 +0800

    chore: update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,111 @@
 import { PrismaClient } from '@prisma/client'
+import dayjs from 'dayjs'
 const prisma = new PrismaClient()
 async function main() {
-    await prisma.user.upsert({
-        where:{
-            uid:'0'
-        },
-        update:{},
-        create:{
-            uid:'uid',
-            uname:'游客'
-        }
-    })
-  
- 
+  const datetime = dayjs().format("YYYY-MM-DD HH:mm:ss")
+  await prisma.sysDept.upsert({
+    where: {
+      deptId: 100
+    },
+    update: {},
+    create: {
+      parentId: 0,
+      ancestors: '0',
+      deptName: '若依科技',
+      orderNum: 0,
+      leader: '若依',
+      phone:"15888888888",
+      email:"ry@qq.com",
+      createBy: "admin",
+      createTime: datetime,
+    }
+  })
+  await prisma.sysDept.upsert({
+    where: {
+      deptId: 101
+    },
+    update: {},
+    create: {
+      parentId: 100,
+      ancestors: '0,100',
+      deptName: '深圳总公司',
+      orderNum: 1,
+      leader: '若依',
+      phone:"15888888888",
+      email:"ry@qq.com",
+      createBy: "admin",
+      createTime: datetime,
+    }
+  })
+  await prisma.sysUser.upsert({
+    where:{
+      userId:1,
+    },
+    update:{},
+    create:{
+      deptId:100,
+      userName:'admin',
+      nickName:'若依',
+      userType:'00',
+      email:"ry@163.com",
+      phonenumber:"15888888888",
+      password:"123456",
+      createBy:'admin'
+    }
+  })
+  await prisma.sysUser.upsert({
+    where:{
+      userId:2,
+    },
+    update:{},
+    create:{
+      deptId:100,
+      userName:'common',
+      nickName:'普通用户',
+      userType:'00',
+      email:"ry@163.com",
+      phonenumber:"15888888888",
+      password:"123456",
+      createBy:'admin'
+    }
+  })
+  await prisma.sysPost.upsert({
+    where:{
+      postId:1
+    },
+    update:{},
+    create:{
+      postCode:"ceo",
+      postName:"董事长",
+      postSort:1,
+      createBy:'admin'
+    }
+  })
+  await prisma.sysRole.upsert({
+    where:{
+      roleId:1
+    },
+    update:{},
+    create:{
+      roleName:'超级管理员',
+      roleKey:'admin',
+      roleSort:1,
+      dataScope:'1'
+    }
+  })
+  await prisma.sysRole.upsert({
+    where:{
+      roleId:2
+    },
+    update:{},
+    create:{
+      roleName:'普通角色',
+      roleKey:'common',
+      roleSort:2,
+      dataScope:'2'
+    }
+  })
+
 }
 main()
   .then(async () => {
