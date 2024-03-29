11d43c26e86f9b0217bcefef59ef51291115a29b
shuTwT <1324073644@qq.com>
2024-03-29 10:43:25 +0800

    chore: update

--- lib/utils/redis.ts
+++ lib/utils/redis.ts
@@ -1,8 +1,18 @@
-import Redis from "ioredis"
+import Redis from "ioredis";
 
-const redis = new Redis({
+const redisClientSingleton = () => {
+  return new Redis({
     port: Number(process.env.REDIS_PORT) || 6789,
-    host: process.env.REDIS_HOST ?? '127.0.0.1'
-})
+    host: process.env.REDIS_HOST ?? "127.0.0.1",
+  });
+};
 
-export default redis
+const globalForPrisma = globalThis as unknown as {
+  redis: Redis | undefined;
+};
+
+const redis = globalForPrisma.redis ?? redisClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.redis = redis;
+
+export default redis;
